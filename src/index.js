d5ae234a987dbd20568744261143e9d9ab0b9814
kshitish <kshitish182@gmail.com>
2020-09-29 15:57:32 +0545

    Add buttons for first and last page

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="hackernewsUI">
       <Home />
     </BrowserRouter>
   </React.StrictMode>,
