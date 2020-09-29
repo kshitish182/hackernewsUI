37fefd51a4a2982fb7d16fa73a0c397ff1e43a47
kshitish <kshitish182@gmail.com>
2020-09-29 15:32:57 +0545

    Updates

---
+++ precache-manifest.8811466f090948c9af5b0bd00f04258c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36c1a59091c91f2d4eee9b2a4ffc3e0",
+    "url": "/hackernewsUI/index.html"
+  },
+  {
+    "revision": "898fec3d3a5fedea2f8a",
+    "url": "/hackernewsUI/static/css/main.5bdcd73e.chunk.css"
+  },
+  {
+    "revision": "363241157499ba5bf7c4",
+    "url": "/hackernewsUI/static/js/2.c066944a.chunk.js"
+  },
+  {
+    "revision": "92bcffc488d6f3fbd12a7720ee4a3b28",
+    "url": "/hackernewsUI/static/js/2.c066944a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "898fec3d3a5fedea2f8a",
+    "url": "/hackernewsUI/static/js/main.92498c8e.chunk.js"
+  },
+  {
+    "revision": "7a8438e38ba47b034149",
+    "url": "/hackernewsUI/static/js/runtime-main.39b7eaf6.js"
+  }
+]);
