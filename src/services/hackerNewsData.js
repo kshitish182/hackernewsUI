d8b55405bf68d12fadc94702d41fc439eaed0142
kshitish182 <kshitish182@gmail.com>
2023-08-31 22:34:54 +0545

    fix routing issue

--- src/services/hackerNewsData.js
+++ src/services/hackerNewsData.js
@@ -6,11 +6,10 @@ import { get } from '../utils/http';
  *
  *  @returns {data[]} Contains all the top stories id in a array
  */
-
 export const getAllData = async () => {
   const ids = await get('/topstories.json?print=pretty');
   if (!ids) {
-    return console.log('empty');
+    return;
   }
 
   return ids;
@@ -20,16 +19,7 @@ export const getNewsItem = async (newsId) => {
   const item = await get(`/item/${newsId}.json?print+pretty`);
 
   if (!item) {
-    return console.log('empty');
+    return;
   }
   return item;
 };
-
-// export const getAllData = () =>
-//   http
-//     .get('/topstories.json?print=pretty')
-//     .then((data) => {
-//       console.log(data.data.length, data.data);
-//       sortToObj(data.data);
-//     })
-//     .catch((err) => console.log(err));
